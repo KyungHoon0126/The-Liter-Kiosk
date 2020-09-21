f7dcc1c2f5aeea13df5be51df8b0297ec0caf8ff
김경훈 <kidi052812@gmail.com>
2020-09-21 14:40:36 +0900

    [ADD] : SplashScreen, Video

---
+++ THE_LITER_KIOSK/THE_LITER_KIOSK/Views/SplashScreen.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace THE_LITER_KIOSK.View
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+            Loaded += SplashScreen_Loaded;
+        }
+
+        private void SplashScreen_Loaded(object sender, RoutedEventArgs e)
+        {
+            dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 3);
+            dispatcherTimer.Start();
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            dispatcherTimer.Stop();
+            this.Close();
+        }
+    }
+}
