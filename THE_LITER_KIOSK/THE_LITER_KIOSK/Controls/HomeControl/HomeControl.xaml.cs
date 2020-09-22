8d57da65678793691d146c3fe45cfe8547066a71
권순관 <gamawoo10@gmail.com>
2020-09-22 23:30:10 +0900

    [ADD] TheLitterVideo AutoPlay

---
+++ THE_LITER_KIOSK/THE_LITER_KIOSK/Controls/HomeControl/HomeControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+            
+namespace THE_LITER_KIOSK.Controls.HomeControl
+{
+    /// <summary>
+    /// HomeControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class HomeControl : UserControl
+    {
+        public HomeControl()
+        {
+            InitializeComponent();
+            MedaiElementVideoPlayer();
+        }
+
+        private void MedaiElementVideoPlayer()
+        {
+            media.Play();
+        }
+
+
+       
+    }
+}
