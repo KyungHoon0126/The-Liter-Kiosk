16aa16516b4d5388bb1efa250e26e1cb59102cc1
권순관 <gamawoo10@gmail.com>
2020-10-05 14:53:48 +0900

    [ADD] PlaceDatas

--- THE_LITER_KIOSK/TheLitter.Core.Place/PlaceData.cs
+++ THE_LITER_KIOSK/TheLitter.Core.Place/PlaceData.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheLitter.Core.Place.ViewModel;
 
 namespace TheLitter.Core.Place
 {
     public class PlaceData
     {
+       public TableViewModel tableViewModel = new TableViewModel();
+
     }
 }
--- THE_LITER_KIOSK/TheLitter.Core.Place/ViewModel/TableViewModel.cs
+++ THE_LITER_KIOSK/TheLitter.Core.Place/ViewModel/TableViewModel.cs
@@ -1,12 +1,97 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheLiter.Core.Order.Model;
+using TheLiter.Core.Order.ViewModel; 
+using TheLitter.Core.Place.Model;
 
 namespace TheLitter.Core.Place.ViewModel
 {
-    class TableViewModel
+    public class TableViewModel: BindableBase
     {
+        OrderViewModel orderViewModel = new OrderViewModel();
+
+        private bool _isActive;
+        public bool IsActive
+        {
+            get => _isActive;
+            set => SetProperty(ref _isActive, value);
+        }
+
+        private ObservableCollection<TableModel> _tableItems = new ObservableCollection<TableModel>();
+        public ObservableCollection<TableModel> TableItems
+        {
+            get => _tableItems;
+            set => SetProperty(ref _tableItems, value);
+        }
+
+        private async Task LoadTableData()
+        {
+            await Task.Run(() =>
+            {
+                #region TableItems
+                var menus = new ObservableCollection<MenuModel>(orderViewModel.MenuItems);
+                TableItems.Add(new TableModel()
+                {
+                    TableIdx = 1,
+                    TotalPrice = 0,
+                    MenuList = menus,
+                });
+                TableItems.Add(new TableModel()
+                {
+                    TableIdx = 2,
+                    TotalPrice = 0,
+                    MenuList = menus,
+                });
+                TableItems.Add(new TableModel()
+                {
+                    TableIdx = 3,
+                    TotalPrice = 0,
+                    MenuList = menus,
+                });
+                TableItems.Add(new TableModel()
+                {
+                    TableIdx = 4,
+                    TotalPrice = 0,
+                    MenuList = menus,
+                });
+                TableItems.Add(new TableModel()
+                {
+                    TableIdx = 5,
+                    TotalPrice = 0,
+                    MenuList = menus,
+                });
+                TableItems.Add(new TableModel()
+                {
+                    TableIdx = 6,
+                    TotalPrice = 0,
+                    MenuList = menus,
+                });
+                TableItems.Add(new TableModel()
+                {
+                    TableIdx = 7,
+                    TotalPrice = 0,
+                    MenuList = menus,
+                });
+                TableItems.Add(new TableModel()
+                {
+                    TableIdx = 8,
+                    TotalPrice = 0,
+                    MenuList = menus,
+                });
+                TableItems.Add(new TableModel()
+                {
+                    TableIdx = 9,
+                    TotalPrice = 0,
+                    MenuList = menus,
+                });
+                #endregion
+            });
+            
+        }
     }
 }
