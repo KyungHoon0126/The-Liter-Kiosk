ffed2e3d6dc3d65bd0af1e39ee7108b8e3d943de
김경훈 <kidi052812@gmail.com>
2020-10-28 11:14:45 +0900

    [ADD] : 회원가입

--- THE_LITER_KIOSK/TheLiter.Core.Member/ViewModel/MemberViewModel.cs
+++ THE_LITER_KIOSK/TheLiter.Core.Member/ViewModel/MemberViewModel.cs
@@ -1,58 +1,138 @@
 ﻿using Prism.Commands;
-using Prism.Mvvm;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
 using System.Windows.Input;
+using TheLiter.Core.DBManager;
+using TheLiter.Core.Member.Model;
 
 namespace TheLiter.Core.Member.ViewModel
 {
-    public class MemberViewModel : BindableBase
+    public class MemberViewModel : MySqlDBConnectionManager, INotifyPropertyChanged
     {
+        private DBManager<MemberModel> memberDBManager = new DBManager<MemberModel>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         #region Properties
         private string _id;
         public string Id
         {
             get => _id;
-            set => SetProperty(ref _id, value);
+            set
+            {
+                _id = value;
+                NotifyPropertyChanged(nameof(Id));
+            }
         }
 
         private string _pw;
         public string Pw
         {
             get => _pw;
-            set => SetProperty(ref _pw, value);
+            set
+            {
+                _pw = value;
+                NotifyPropertyChanged(nameof(Pw));
+            }
         }
 
         private string _qrCode;
         public string QrCode
         {
             get => _qrCode;
-            set => SetProperty(ref _qrCode, value);
+            set
+            {
+                _qrCode = value;
+                NotifyPropertyChanged(nameof(QrCode));
+            }
         }
 
         private string _barCode;
         public string BarCode
         {
             get => _barCode;
-            set => SetProperty(ref _barCode, value);
+            set
+            {
+                _barCode = value;
+                NotifyPropertyChanged(nameof(BarCode));
+            }
         }
         #endregion
 
         #region Command
-        public ICommand SignUpCommand;
+        public ICommand SignUpCommand { get; set; }
         #endregion
 
+        #region Constructor
         public MemberViewModel()
         {
-            SignUpCommand = new DelegateCommand(OnSignUp, CanSignUp);
+            SignUpCommand = new DelegateCommand(OnSignUp, CanSignUp).ObservesProperty(() => BarCode);
         }
+        #endregion
 
-        private void OnSignUp()
+        #region Command Method
+        private async void OnSignUp()
         {
+            try
+            {
+                using (IDbConnection db = GetConnection())
+                {
+                    db.Open();
 
+                    var memberModel = new MemberModel();
+                    memberModel.BarCode = BarCode;
+                    memberModel.QrCode = QrCode;
+                    memberModel.Id = Id;
+                    memberModel.Pw = Pw;
+
+                    string insertSql = @"
+INSERT INTO member_tb(
+    BarCode,
+    QrCode,
+    Id,
+    Pw
+)
+VALUES(
+    @qrCode,
+    @barCode,
+    @id,
+    @pw
+);";
+                    if (await memberDBManager.InsertAsync(db, insertSql, memberModel) == 1)
+                    {
+                        Debug.WriteLine("SUCCESS SIGN UP");
+                        ClearSignUpData();
+                    }
+                    else
+                    {
+                        Debug.WriteLine("FAILURE SIGN UP");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Write("SIGN UP ERROR : " + e.Message);
+            }
         }
 
         private bool CanSignUp()
         {
-            return true;
+            return (Id != null) && (Pw != null) && (QrCode != null) && (BarCode != null);
+        }
+        #endregion
+
+        private void ClearSignUpData()
+        {
+            Id = string.Empty;
+            Pw = string.Empty;
+            QrCode = string.Empty;
+            BarCode = string.Empty;
         }
     }
 }
