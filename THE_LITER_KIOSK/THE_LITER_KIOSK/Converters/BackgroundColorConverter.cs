cc3056d48ab53178be1e4ef788e70f6c206634ef
권순관 <gamawoo10@gmail.com>
2020-11-02 23:56:17 +0900

    [ADD] Table Complete

---
+++ THE_LITER_KIOSK/THE_LITER_KIOSK/Converters/BackgroundColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace THE_LITER_KIOSK.Converters
+{
+    class BackgroundColorConverter : IValueConverter
+    {
+      public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool)value) ? Brushes.Red : Brushes.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
